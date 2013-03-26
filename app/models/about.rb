b9e6205ad782d5c20a85455c332c283489e3b2d9
Pavle Jovanovic <pavlej@vailsys.com>
2013-03-26 02:34:48 -0500

    resume fix

--- app/models/about.rb
+++ app/models/about.rb
@@ -1,4 +1,4 @@
 class About < ActiveRecord::Base
   attr_accessible :content, :resume, :phone, :email
-  has_attached_file :resume, :url => ":s3_domain_url", :path => "/:class/resume/:id_:basename.:extension"
+  has_attached_file :resume, :url => ":s3_domain_url", :path => "/:class/resume/:id_:basename.:style.:extension"
 end
