b1d2f7b27a32957ce57ca60772ec0f4b3f4620fe
Pavle Jovanovic <pavlej@vailsys.com>
2013-03-25 23:21:46 -0500

    fixing

--- app/models/illustration.rb
+++ app/models/illustration.rb
@@ -2,5 +2,5 @@ class Illustration < ActiveRecord::Base
   default_scope order('placement ASC')
   attr_accessible :description, :title, :image, :placement
   validates_attachment_presence :image
-  has_attached_file :image, :styles => { :large => "800x800", :medium => "300x300>", :thumb => "100x100>", :url => "/:class/:attachment/:id_partition/:style/:filename" }
+  has_attached_file :image, :styles => { :large => "800x800", :medium => "300x300>", :thumb => "100x100>" }, :url => "/:class/:attachment/:id_partition/:style/:filename"
 end
